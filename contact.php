38ccb7328ff500dc8390ff066964f25cd5072d0c
Chaiwonkeat <wonkeat990331@gmail.com>
2021-12-28 10:26:46 +0800

    news
    news

--- contact.php
+++ contact.php
@@ -1,3 +1,17 @@
+<?php
+if(isset($_POST['submitSave'])) {
+	$signup = simplexml_load_file('contact.xml');
+	$product = $signup->addChild('customer');
+	$product->addChild('firstname', $_POST['firstname']);
+	$product->addChild('lastname', $_POST['lastname']);
+    $product->addChild('email', $_POST['email']);
+    $product->addChild('phone', $_POST['phone']);
+    $product->addChild('comments', $_POST['comments']);
+	file_put_contents('contact.xml', $signup->asXML());
+	//header('location:index.php');
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
    <head>
@@ -9,7 +23,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="viewport" content="initial-scale=1, maximum-scale=1">
       <!-- site metas -->
-      <title>Eflyer</title>
+      <title>Jewellery</title>
       <meta name="keywords" content="">
       <meta name="description" content="">
       <meta name="author" content="">
@@ -35,76 +49,94 @@
       <link rel="stylesheet" href="css/owl.carousel.min.css">
       <link rel="stylesoeet" href="css/owl.theme.default.min.css">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
-      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
       <style>
-         .star-icon {
-            color: #ffe400;
-            font-size: 20px;
-            font-family: sans-serif;
-            font-weight: 800;
+        body {
+            color: #999;
+            background: #fafafa;
+            font-family: 'Roboto', sans-serif;
+        }
+        .form-control {
+            min-height: 41px;
+            box-shadow: none;
+            border-color: #e6e6e6;
+        }
+        .form-control:focus {
+            border-color: #00c1c0;
+        }
+        .form-control, .btn {        
+            border-radius: 3px;
+        }
+        .signup-form {
+            width: 550px;
+            margin: 0 auto;
+            padding: 30px 0;
+            padding-top:550px;
+        }
+        .signup-form h2 {
+            color: #333;
+            font-weight: bold;
+            margin: 0 0 25px;
+              text-align: center;
+        }
+        .signup-form h5 {
+            color: black;
+        }
+        .signup-form form {
+            margin-bottom: 15px;
+            background: #fff;
+            border: 1px solid #f4f4f4;
+            box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+            padding: 40px 100px;
+        }
+        .signup-form .form-group {
+            margin-bottom: 20px;
+        }
+        .signup-form label {
+            font-weight: normal;
+            font-size: 14px;
+        }
+        .signup-form input[type="checkbox"] {
+            position: relative;
+            top: 1px;
+        }    
+        .signup-form .btn, .signup-form .btn:active {        
+            font-size: 16px;
+            font-weight: bold;
+            background: #00c1c0 !important;
+            border: none;
+            min-width: 140px;
+        }
+        .signup-form .btn:hover, .signup-form .btn:focus {
+            background: #00b3b3 !important;
+         }
+        .signup-form a {
+            color: #00c1c0;
+            text-decoration: none;
+         }	
+        .signup-form a:hover {
+            text-decoration: underline;
+         }
+        .form-group{
             text-align: center;
-            text-transform: uppercase;
-            padding: 20px 0;
-            }
-            .rating-css label {
-            font-size: 30px;
-            text-shadow: 1px 1px 0 #ffe400;
-            cursor: pointer;
-            }
-
-            .rating-css label:active {
-            transform: scale(0.8);
-            transition: 0.3s ease;
-            }
-            #jewellery_main_slider a.carousel-control-prev{
-               top:85%;
-            }
-            #jewellery_main_slider a.carousel-control-next{
-               top:85%;
-            }
-            .inside{
-               margin-top:50px;
-               margin-bottom:50px;
-            }
-            #jewellery_main_slider .carousel-control-next, #jewellery_main_slider .carousel-control-prev{
-               margin-top:150px;
-            }
-            .linktext li a{
-               color: #212529 !important;
-            }
-            @media (min-width: 768px) and (max-width: 991px){
-               #jewellery_main_slider .carousel-control-next, #jewellery_main_slider .carousel-control-prev{
-                  margin-top:120px;
-               }
-               .dropdown{
-					   display:block;
-			   }
-            }
-            @media (max-width: 575px){
-               #jewellery_main_slider .carousel-control-next, #jewellery_main_slider .carousel-control-prev{
-                  margin-top: 380px;
-               }
-               .login_menu li{
-					   font-size:14pt;
-			      }
-			      .dropdown{
-				      display:block;
-			      }
-               .login_menu li{
-                  padding-left:0px;
-               }
-               .login_menu ul{
-                  padding:0px;
-                  margin:0px;
-               }
-            }
-
-      </style>
-   </head>
+         }
+        .tex{
+            text-align: center;
+         }
+         .ter{
+            padding-bottom:40px;
+         }
+         .bot{
+            padding: 8px 80px;
+         }
+        </style>
+    </head>
    <body>
       <!-- banner bg main start -->
       <div class="banner_bg_main">
+         <!-- header top section start -->
+         
+         <!-- header top section start -->
+         <!-- logo section start -->
          <div class="logo_section">
             <div class="container">
                <div class="row">
@@ -126,7 +158,7 @@
                      <a href="men.html">Men</a>
                      <a href="women.html">Women</a>
                      <a href="#">About</a>
-                     <a href="#">Contact</a>
+                     <a href="contact.html">Contact</a>
                   </div>
                   <span class="toggle_icon" onclick="openNav()"><img src="images/toggle-icon.png"></span>
                   <div class="main">
@@ -145,38 +177,12 @@
                         <ul>
                            <li><a href="#">
                               <i class="fa fa-shopping-cart" aria-hidden="true"></i>
-                         
-                              <span class="padding_10">Cart <span class="badge badge-secondary">     <?php
-               $products = simplexml_load_file('product.xml'); //load the files xml . 
-                echo count($products); // count the number products
-               // echo '<br>List Product Information';
-                ?></span></span></a>
+                              <span class="padding_10">Cart</span></a>
                            </li>
-                           <?php
-
-                           if(!isset($_COOKIE['user'])){?>
-                           <li><a href="login.php">
+                           <li><a href="#">
                               <i class="fa fa-user" aria-hidden="true"></i>
                               <span class="padding_10">Login</span></a>
                            </li>
-                           <?php }else{ ?>
-                              <li><a href="login.php">
-                              <i class="fa fa-user" style="display:none;" aria-hidden="true"></i>
-                        <div class="dropdown">
-                           <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-                           <span class="padding_10 "><?php echo $_COOKIE['user']; ?></span>
-                           </a>
-
-                           <ul class="dropdown-menu linktext" aria-labelledby="dropdownMenuLink">
-                              <li><a class="btn btn-secondary dropdown-item" href="profile.php">Account</a></li>
-                              <li><a class="btn btn-secondary dropdown-item" href="#">Payment</a></li>
-                              <li><a class="btn btn-secondary dropdown-item" href="#">History</a></li>
-                              <li><a class="btn btn-secondary dropdown-item" href="#">Log Out</a></li>
-                           </ul>
-                        </div></a>
-							   </li>
-                           <?php }
-                           ?>
                         </ul>
                      </div>
                   </div>
@@ -192,24 +198,24 @@
                      <div class="carousel-item active">
                         <div class="row">
                            <div class="col-sm-12">
-                              <h1 class="banner_taital">News <br>Product</h1>
-                              <div class="buynow_bt"><a href="news.html">SHOP NOW</a></div>
+                              <h1 class="banner_taital">Get Start <br>Your favriot shoping</h1>
+                              <div class="buynow_bt"><a href="#">Buy Now</a></div>
                            </div>
                         </div>
                      </div>
                      <div class="carousel-item">
                         <div class="row">
                            <div class="col-sm-12">
-                              <h1 class="banner_taital">Men <br>Product</h1>
-                              <div class="buynow_bt"><a href="men.html">SHOP NOW</a></div>
+                              <h1 class="banner_taital">Get Start <br>Your favriot shoping</h1>
+                              <div class="buynow_bt"><a href="#">Buy Now</a></div>
                            </div>
                         </div>
                      </div>
                      <div class="carousel-item">
                         <div class="row">
                            <div class="col-sm-12">
-                              <h1 class="banner_taital">Women <br>Product</h1>
-                              <div class="buynow_bt"><a href="women.html">SHOP NOW</a></div>
+                              <h1 class="banner_taital">Get Start <br>Your favriot shoping</h1>
+                              <div class="buynow_bt"><a href="#">Buy Now</a></div>
                            </div>
                         </div>
                      </div>
@@ -226,53 +232,46 @@
          <!-- banner section end -->
       </div>
       <!-- banner bg main end -->
-      <!-- fashion section start -->
-      <div class="fashion_section">
-         <div id="main_slider" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-               <?php
-              // $products = simplexml_load_file('product.xml'); //load the files xml . 
-                echo 'Number of products: '.count($products); // count the number products
-               // echo '<br>List Product Information';
-                ?>
-               <div class="carousel-item active">
-                  <div class="container">
-                     <h1 class="fashion_taital">Product</h1>
-                     <div class="fashion_section_2">
-                        <div class="row">   <?php foreach($products as $product) {?>
-                           <div class="col-lg-4 col-sm-4">
-                              <div class="box_main">
-                                 <h4 class="shirt_text"><?php echo $product->name; //echo 'Number of products: '.count($news);
-                                
-                                    ?></h4>
-                                 <div class="tshirt_img"><img src="<?php echo $product->path; ?>"></div>
-                                 <p class="price_text">Price : <span style="color: #262626;">RM<?php echo $product->price; ?></span></p>
-                                 <div class="rating-css">
-                                    <div class="star-icon">
-                                      <label for="rating1" class="fa fa-star"></label>
-                                      <label for="rating2" class="fa fa-star"></label>
-                                      <label for="rating3" class="fa fa-star"></label>
-                                      <label for="rating4" class="fa fa-star"></label>
-                                    </div>
-                                  </div>
-                                 <div class="btn_main">
-                                    <div class="buy_bt"><a href="index.php/?id=<?php echo $products->product['id']; ?>">Buy Now</a></div>
-                                    <div class="seemore_bt"><a href="product_details/?id=<?php echo $products->product['id']; ?>">See More</a></div>
-                                 </div>
-                              </div>
-                           </div>
-                           
-                             <?php 
-                             } ?>
-                        </div>
-                     </div>
-                  </div>
-               </div>
-            </div>
+      <!-- jewellery  section start -->
+      <div class="container">
+    <div class="row">
+        <h1>Contact Us!</h1>
+        <form class="row g-3" action="" method="POST">
+          <!-- Honeypot -->
+          <input type="text" name="_honey" style="display: none;">
 
-         </div>
+          <!-- Disable Captcha -->
+          <input type="hidden" name="_captcha" value="false">
+
+          <input type="hidden" name="_next" value="success.html" >
+
+          <div class="col-xs-6 col-md-4">
+            <label for="firstName" class="form-label">First Name</label>
+            <input type="firstname" class="form-control" name="firstname" id="firstName" required>
+          </div>
+          <div class="col-md-6">
+            <label for="lastName" class="form-label">Last Name</label>
+            <input type="lastname" class="form-control" name="lastname" id="lastName" required>
+          </div>
+          <div class="col-md-8">
+            <label for="emailInfo" class="form-label">E-mail</label>
+            <input type="email" class="form-control" name="email" id="emailInfo" required>
+          </div>
+          <div class="col-md-4">
+            <label for="phoneNumber" class="form-label">Phone Number</label>
+            <input type="text" class="form-control" name="phone" id="phoneNumber" placeholder="phone number" pattern="[0-9]{3}-[0-9]{2}[0-9]{5}">
+          </div>
+          <div class="col-md-12">
+            <label for="comments" class="form-label">Comments, questions?</label>
+            <textarea class="form-control" id="comments" name="comments,&nbsp;questions" rows="3" required cols="50" minlength="3" maxlength="30"></textarea>
+          </div>
+          <div class="col-md-12">
+            <button type="submit" class="btn btn-primary">Submit</button>
+          </div>
+        </form>
       </div>
-
+      <!-- jewellery  section end -->
+      <!-- footer section start -->
       <div class="footer_section layout_padding">
          <div class="container">
             <div class="footer_logo"><a href="index.html"><img src="images/footer-logo.png"></a></div>
@@ -292,7 +291,7 @@
             <div class="location_main">Help Line  Number : <a href="#">(+060) 120 2200</a></div>
          </div>
       </div>
-      <!-- footer section end -->
+      <!-- copyright section end -->
       <!-- Javascript files-->
       <script src="js/jquery.min.js"></script>
       <script src="js/popper.min.js"></script>
@@ -312,4 +311,4 @@
          }
       </script>
    </body>
-</html>s
+</html>
